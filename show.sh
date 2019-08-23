6da8d7b924cb4e5103627eae8cb0594b08235a42
aimlauer <suurrend3r@gmail.com>
2019-08-23 11:52:35 -0300

    arreglo el script

--- show.sh
+++ show.sh
@@ -3,7 +3,8 @@
 File="GGG.py"
 NOP=5
 Dir="ejemplos"
-echo -ne "\e[1mLeyendo archivos desde $Dir/: \033[0m \n"
+
+echo -ne "\e[1mLeyendo archivos desde la carpeta \e[42m$Dir\e[0m: \e[0m \n"
 for i in $(ls $Dir); do 
   echo -ne "\033[34m$i\033[0m, "
   (
