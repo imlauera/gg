b73e56b0f97295ef48a98d6ec8ddadc1d19b8e2f
aimlauer <suurrend3r@gmail.com>
2019-08-23 11:38:11 -0300

    arreglo el script

--- show.sh
+++ show.sh
@@ -4,20 +4,12 @@ File="GGG.py"
 NOP=5
 Dir="ejemplos"
 
-#for i in $(ls $Dir); do 
-#  python $File2 $Dir/$i  &
-#  PID=$!
-#  sleep $NOP; kill -9 $PID
-#done
-
-
-
-echo -ne "\033[92mLeyendo archivos: $i \033[0m"
+echo -ne "\033[1mLeyendo archivos desde $Dir/: \033[0m \n"
 for i in $(ls $Dir); do 
-  echo -e "\033[91m$i \033[0m"
+  echo -ne "\033[91m$i\033[0m, "
   (
     sleep $NOP;
-    echo -e "\n"
-  ) | python2 $File $Dir/$i  
+    echo -e '\n'
+  ) | python2 $File $Dir/$i > /dev/null || break
 done
 
