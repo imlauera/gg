35dbcdf82969bdda5425c2f6c20b698abf2be5f2
aimlauer <suurrend3r@gmail.com>
2019-08-24 02:56:22 -0300

    retoquues

--- GGG.py
+++ GGG.py
@@ -125,7 +125,7 @@ class LayoutGraph:
   def create_view(self):
     self.gplot = gp.Gnuplot()
 
-    size = "set term {} linewidth 6.5 size {},{}".format(self.term,self.width,self.height)
+    size = "set term {} linewidth 7 size {},{}".format(self.term,self.width,self.height)
     # size = "set term {} size {},{}".format(self.term,self.width,self.height)
     self.gplot(size)
 
